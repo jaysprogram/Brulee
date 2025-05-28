eeb26696f00f5318fd43d7d8614343988400a730
ale0614 <alrenellij@gmail.com>
2025-05-27 21:33:57 -0400

    Add checkout page

---
+++ src/app/checkout/umaylike.js
@@ -0,0 +1,25 @@
+// /data/recommended.js
+const allProducts = [
+  {
+    id: "braceletSet",
+    name: "Bracelet Set",
+    image: "/images/jewelry-pic/braceletSet.png",
+  },
+  {
+    id: "diamondBracelet",
+    name: "Diamond Bracelet",
+    image: "/images/jewelry-pic/diamondBracelet.png",
+  },
+  {
+    id: "goldBracelet",
+    name: "Gold Bracelet",
+    image: "/images/jewelry-pic/goldBracelet.png",
+  },
+  {
+    id: "awesomeWidget",
+    name: "Awesome Widget",
+    image: "/images/widget.jpg",
+  },
+];
+
+export default allProducts;
